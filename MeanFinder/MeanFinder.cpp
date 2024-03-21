4ac397ce5d0a5ca0ac471e9ee48e77f196c4fc72
projectFirehive <projectFirehive@gmail.com>
2024-03-21 20:59:26 +0000

    Updated to find number of elements in array instead of size in bytes.

--- MeanFinder/MeanFinder.cpp
+++ MeanFinder/MeanFinder.cpp
@@ -8,11 +8,12 @@ using namespace std;
 int main()
 {
     double inputList[] = { 30, 90, 60, 70, 5, 1, 1, 50, 1, 50, 80, 30, 1, 10, 85, 10, 50, 20, 50, 50, 30, 1, 30, 40, 30, 20, 50, 30, 98, 35 };
+    int inputListSize = sizeof(inputList) / sizeof(double);
     double sum = 0;
     for (auto& num : inputList) {
         sum += num;
     }
-    double mean = (sum / sizeof(inputList));
+    double mean = (sum / inputListSize);
     cout << mean;
     return 0;
 }
